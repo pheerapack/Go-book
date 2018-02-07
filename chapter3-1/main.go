893deda347f1666abbbafdeea0c54190a842e099
pheerapack <h.deleted.p@gmail.com>
2018-02-07 10:33:41 +0700

    Add chapter 3

---
+++ chapter3-1/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("---ZERO VALUE---")
+	var number int
+	var str string
+	var boolean bool
+	fmt.Printf("Number : %v\n", number)
+	fmt.Printf("Str : '%v'\n", str)
+	fmt.Printf("Boolean : %v\n", boolean)
+}
