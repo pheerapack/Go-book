8e1ef6d04c01e60267ae7630d182236c1d0ff1f3
pheerapack <h.deleted.p@gmail.com>
2018-02-08 10:57:27 +0700

    C degree to F degree

--- chapter4-2/main.go
+++ chapter4-2/main.go
@@ -5,10 +5,12 @@ func main() {
 	var input float64
 	fmt.Scanf("%f", &input)
 	output := (( 5 *(input - 32.0)) / 9.0)
-	//a := float64(output)
-	fmt.Printf("%.2f",output)
+	//a= input
+	fmt.Println(input)
+	//fmt.Sprintf("%.2f",input)
+	fmt.Printf("Fahrenheit into Celsius : %.2f",output)
+	fmt.Sprintf("%.6f", 21312421.213123)
 	
-
 	//i := 5
    // f := float64(i)
    // fmt.Printf("f is %f\n", f)
