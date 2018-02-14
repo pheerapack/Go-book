92bcf1b52f73eca1741ed0fa4c73ebe7f65a65ec
pheerapack <h.deleted.p@gmail.com>
2018-02-14 10:27:31 +0700

    rewrite fizzbuzz to function

---
+++ chapter5-2/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main()  {
+	for j := 1; j <= 100; j++ {
+		r := findfizzbuzz(j)
+		fmt.Println(r)
+	}
+}
+
+func findfizzbuzz(j int) (r string) {
+	if j%15 == 0 {
+		r = "FizzBuzz"
+	} else if j%3 == 0 {
+		r = "Fizz"
+	} else if j%5 == 0 {
+		r = "Buzz"
+	} else {
+		r = "Number"
+	}
+	return r
+}
