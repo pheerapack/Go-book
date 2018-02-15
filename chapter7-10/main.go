c649360f3ff6cf6bfa31c9cdd6a35cc77202afda
pheerapack <h.deleted.p@gmail.com>
2018-02-15 16:48:24 +0700

    trst

--- chapter7-10/main.go
+++ chapter7-10/main.go
@@ -3,13 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	maps := map[string]int {
-		"one":1,
-		"two":2,
-		"tree":3,
+	maps := map[string]int{
+		"one":  1,
+		"two":  2,
+		"tree": 3,
 	}
 
-	for key,number := range maps {
-		fmt.Println(key,number)
+	for key, number := range maps {
+		fmt.Println(key, number)
+
 	}
 }
