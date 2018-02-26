da0cac34dac85d5de73045c0ef8189f29aa33f92
pheerapack <h.deleted.p@gmail.com>
2018-02-26 11:00:50 +0700

    change func

--- execerise/ven3.go
+++ execerise/ven3.go
@@ -8,7 +8,24 @@ import (
 	"strings"
 )
 
+type VendingMachine struct {
+	insertedMoney int
+	coins         map[string]int
+}
+
+func (m VendingMachine) InsertedMoney() int {
+	return m.insertedMoney
+}
+
+func (m *VendingMachine) InsertCoin(coin string) {
+	m.insertedMoney += m.coins[coin]
+}
+
 func main() {
+	var coins = map[string]int{"T": 10, "F": 5, "TW": 2, "O": 1}
+	vm := VendingMachine{coins: coins}
+	fmt.Println("Insert Money:", vm.InsertedMoney())
+
 	snr := bufio.NewScanner(os.Stdin)
 	enter := "Enter a line of data:"
 	for fmt.Println(enter); snr.Scan(); fmt.Println(enter) {
@@ -18,6 +35,7 @@ func main() {
 		}
 		fields := strings.Fields(line)
 		fmt.Printf("Fields: %q\n", fields)
+
 	}
 	if err := snr.Err(); err != nil {
 		if err != io.EOF {
--- execerise/vending-machine.go
+++ execerise/vending-machine.go
@@ -15,30 +15,40 @@ func (m *VendingMachine) InsertCoin(coin string) {
 	m.insertedMoney += m.coins[coin]
 }
 
-/*
-func (c NewVendingMachine) InsertCoin() string {
-	maps := map[string]string{
-		"T": "10",
-		"F": "5",
-		"O": "1",
-	}
-	return maps[c.coins]
+func (m *VendingMachine) SelectSD() string {
+	m.insertedMoney = 0
+	return "SD"
 }
-*/
+func (m *VendingMachine) SelectCC() string {
+	price := 12
+	change := m.insertedMoney - price
+	return "CC" + m.change(change)
+}
+
+func (m VendingMachine) change(c int) (change string) {
+	return ",F,TW,O"
+}
 func main() {
-	vm := VendingMachine{}
+	var coins = map[string]int{"T": 10, "F": 5, "TW": 2, "O": 1}
+	vm := VendingMachine{coins: coins}
 	fmt.Println("Insert Money:", vm.InsertedMoney())
 	vm.InsertCoin("T")
 	vm.InsertCoin("F")
 	vm.InsertCoin("TW")
 	vm.InsertCoin("O")
 	fmt.Println("Insert Money:", vm.InsertedMoney())
-	//vm.InsertCoin("T")
-	//vm.InsertCoin("F")
-	//vm.InsertCoin("TW")
-	//vm.InsertCoin("O")
-	//fmt.Println("Inserted Money:", vm.GetInsertedMoney()) // 18
+	can := vm.SelectSD()
+	fmt.Println(can)
 
-	//can := vm.SelectSD()
-	//fmt.Println(can) // SD
+	vm.InsertCoin("T")
+	vm.InsertCoin("F")
+	fmt.Println("Insert Money:", vm.InsertedMoney())
+	can = vm.SelectCC()
+	fmt.Println(can)
+
+	vm.InsertCoin("T")
+	vm.InsertCoin("T")
+	fmt.Println("Insert Money:", vm.InsertedMoney())
+	can = vm.SelectCC()
+	fmt.Println(can)
 }
