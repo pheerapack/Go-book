1b35e802247a493997de538e17a8693ebd2e8e63
pheerapack <h.deleted.p@gmail.com>
2018-02-28 10:04:54 +0700

    execerise vending

--- execerise/vending-machine.go
+++ execerise/vending-machine.go
@@ -1,62 +1,14 @@
 package main
 
 import "fmt"
+import "vendingmachine"
 
-type VendingMachine struct {
-	insertedMoney int
-	coins         map[string]int
-	items         map[string]int
-}
+var coins = map[string]int{"T": 10, "F": 5, "TW": 2, "O": 1}
+var items = map[string]int{"SD": 18, "CC": 12}
 
-func (m VendingMachine) InsertedMoney() int {
-	return m.insertedMoney
-}
-
-func (m *VendingMachine) InsertCoin(coin string) {
-	m.insertedMoney += m.coins[coin]
-}
-
-func (m *VendingMachine) SelectSD() string {
-	//m.insertedMoney = 0
-	price := m.items["SD"]
-	change := m.insertedMoney - price
-	m.insertedMoney = 0
-	return "SD" + m.change(change)
-}
-func (m *VendingMachine) SelectCC() string {
-	//m.insertedMoney = 0
-	price := m.items["CC"]
-	change := m.insertedMoney - price
-	m.insertedMoney = 0
-	return "CC" + m.change(change)
-}
-
-func (m *VendingMachine) CoinsReturn() string {
-	coins := m.change(m.insertedMoney)
-	m.insertedMoney = 0
-	return coins[2:len(coins)]
-}
-
-func (m *VendingMachine) change(c int) string {
-	var str string
-	values := [...]int{10, 5, 2, 1}
-	coins := [...]string{"T", "F", "TW", "O"}
-
-	for i := 0; i < len(values); i++ {
-		if c >= values[i] {
-			str += "," + coins[i]
-			c -= values[i]
-			i--
-			//Change 10,5,2,1 asc
-		}
-
-	}
-	return str
-}
 func main() {
-	var coins = map[string]int{"T": 10, "F": 5, "TW": 2, "O": 1}
-	var items = map[string]int{"SD": 18, "CC": 12}
-	vm := VendingMachine{coins: coins, items: items}
+
+	vm := vendingmachine.NewVendingMachine(coins, items)
 	fmt.Println("Insert Money:", vm.InsertedMoney())
 	vm.InsertCoin("T")
 	vm.InsertCoin("F")
@@ -82,7 +34,7 @@ func main() {
 	vm.InsertCoin("T")
 	vm.InsertCoin("TW")
 	vm.InsertCoin("F")
-	fmt.Println("Inserted Money 5:", vm.InsertedMoney())
+	fmt.Println("Inserted Money:", vm.InsertedMoney())
 	coin := vm.CoinsReturn()
 	fmt.Println(coin)
 }
